b857bfffd8272406b531c499cdc8e32b55eb6a76
The Gaming Engineer <gaming_engineer@outlook.com>
2025-03-17 13:20:51 +0100

    opdatering 1

---
+++ MySQL.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 17 09:09:41 2025
+
+@author: spac-30
+"""
+
+import mysql.connector
+import pandas
+import sys
+
+class MySQL:
+    def __init__(self,host,user,password,db):
+        self.host=host
+        self.user=user
+        self.password=password
+        self.db=db
+        self.connection=None
+    
+    def connect(self):
+        try: ## try to make connection
+            self.connection=mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                db=self.db)
+            print("Connection Successful")
+        except:
+            try: 
+                self.connection=mysql.connector.connect(
+                    host=self.host,
+                    user=self.user,
+                    password=self.password)
+                self.connection.cursor().execute(f"CREATE DATABASE IF NOT EXISTS {self.db}")
+                self.connection.commit()
+                self.connection.cursor().close()
+                print("Connection Successful")
+            except mysql.connector.Error as e:
+                print(f"Error:{e}")
+                self.connection=None
+     
+    def close(self):
+        if self.connection:
+            self.connection.close()
+            print("Session Closed")
+        else:
+            print("No Active Session")
+     
+    def create_db(self):
+       if self.connection:
+           
+           db_check=self.connection.cursor()
+           db_check.execute("SHOW DATABASES")
+           self.connection.commit()
+           db_check.close()
+           if self.db in db_check:
+               print(f"Database {self.db} Already Exists.")
+           else:
+               self.connection.cursor().execute(f"CREATE DATABASE {self.db}")
+               self.connection.commit()
+               self.connection.cursor().close()
+       else:
+           print("No Connection Established. Start A Session First.")
+
+    def insert_table(self,name,tabel):
+        if not type(tabel)==pandas.core.frame.DataFrame and ".csv" in tabel:
+            try:
+                data=pandas.read_csv(tabel,sep=",")
+            except FileNotFoundError as e:
+                print(f"File not found: {e}. Either the filename has been given erroneously, path to file has not been provided when needed, or file simply does not exist!")
+            except PermissionError as e:
+                print(f"Permission denied: {e}. Please change permissions of the file and try again.")
+            except IsADirectoryError as e:
+                print(f"Expected a file but found a directory: {e}. This script only handles text-files.")
+            except OSError as e:
+                print(f"OS error: {e}.")
+            except Exception as e:
+                print(f"An unexpected error occurred: {e}.")
+        elif type(tabel)==pandas.core.frame.DataFrame:
+            data=tabel
+        else:
+            print("ERROR: For This either use a .csv file or a pandas dataframe. ")
+            sys.exit()
+        
+        columns=", ".join([f" '{col}' VARCHAR(255)" for col in list(data.columns)])
+        cursor= self.connection.cursor()    
+        cursor.execute(f"CREATE TABLE IF NOT EXISTS '{name}'  ({columns})")
+        self.connection.commit()
+        
+        
+        cursor.execute()
+        
--- opgave_uge_4.py
+++ opgave_uge_4.py
@@ -4,27 +4,18 @@ Created on Mon Mar 17 11:18:50 2025
 
 @author: spac-30
 """
-import connect_MySQL
 
-mydb=connect_MySQ(host="localhost",
+
+from MySQL import MySQL
+import pandas as pd
+
+
+
+mydb=MySQL(host="localhost",
                   user="root",
-                  password=,
-                  db=)
+                  password="Velkommen25",
+                  db="combined_orders")
+mydb.connect()
 
-
-class MySQL:
-    def __init__():
-        self=0
     
-    def create_db(self, name):
-       if self.connection:
-           
-           db_check=self.connection.cursor().execute("SHOW DATABASES")
-           if name in db_check:
-               print(f"Database {name} Already Exists.")
-           else:
-               self.connection.cursor().execute(f"CREATE DATABASE {name}")
-       else:
-           print("No Connection Established. Start A Session First.")
-       
-       
+    
